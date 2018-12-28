8b584129f4d6b5fab31d6fb8f8ba355789892e29
unknown <wangjuanjuan@gc.com>
2018-12-28 16:09:11 +0800

    11

---
+++ day02/02_const/买菜.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	const int price = 3;
+	//price = 5;
+	int weight = 3;
+	int sum = price * weight;
+	printf("%d\n",sum);
+	printf("%d\n", sum);
+	return 0;
+}
